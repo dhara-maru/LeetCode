9f5cd6a9a3cd10fd149bba1596bae412e76d65de
dhara-maru <dharamaru406@gmail.com>
2024-10-10 18:40:19 +0530

    LC1603 : Design Parking System

---
+++ october24/LC1603.java
@@ -0,0 +1,45 @@
+
+// LC 1603 : Design Parking System
+
+class ParkingSystem {
+    int big, medium, small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (carType == 1) {
+            if (big > 0) { // if big is available
+                big--; // now i'll decrement big's one slot
+                return true;
+            }
+        } else if (carType == 2) {
+            if (medium > 0) {
+                medium--;
+                return true;
+            }
+        } else if (carType == 3) {
+            if (small > 0) {
+                small--;
+                return true;
+            }
+        }
+
+        return false; // otherwise return false if nothing is available
+
+    }// method over here
+}
+
+public class LC1603 {
+    public static void main(String[] args) {
+        ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+
+        System.out.println(parkingSystem.addCar(1));
+        System.out.println(parkingSystem.addCar(2));
+        System.out.println(parkingSystem.addCar(3));
+        System.out.println(parkingSystem.addCar(1));
+    }
+}
