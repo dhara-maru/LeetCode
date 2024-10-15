f824cdbf50711b1b74e260862f1ee05a6efd2096
dhara-maru <dharamaru406@gmail.com>
2024-10-15 16:47:02 +0530

    LC185 : Department Top 3 Salaries

---
+++ october24/LC185.sql
@@ -0,0 +1,10 @@
+-- LC 185 : Department Top 3 Salaries
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId
+    AND e2.salary > e.salary
+) < 3;
