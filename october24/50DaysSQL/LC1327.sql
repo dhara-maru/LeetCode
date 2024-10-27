7734cfbeb2e8c049c2ad7cc5d7635f3e8af2e446
dhara-maru <dharamaru406@gmail.com>
2024-10-27 08:48:05 +0530

    Day 19 SQL : List the Products Ordered in a Period

---
+++ october24/50DaysSQL/LC1327.sql
@@ -0,0 +1,8 @@
+-- LC 1327 : List the Products Ordered in a Period
+select p.product_name,  sum(o.unit) as unit
+from Products p
+join Orders o
+on p.product_id = o.product_id
+where o.order_date between '2020-02-01' and '2020-02-29'
+group by p.product_id, p.product_name
+having sum(o.unit) >= 100;
