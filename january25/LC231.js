43e7070f070219e33e9399e44b646481b35e9bd8
dhara-maru <dharamaru406@gmail.com>
2025-01-07 18:51:42 +0530

    LC 231 : Power of Two

---
+++ january25/LC231.js
@@ -0,0 +1,18 @@
+// LC 231 : Power of Two
+var isPowerOfTwo = function(n) {
+    if(n<1){
+        return false;
+    }else if(n==1){
+        return true;
+    }else{
+        while(n%2 ==0){
+            n = n/2;
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
